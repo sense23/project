2fd7516db75565b2b2f6483c76ed76bdcce71a51
Dima <example@gmail.com>
2023-03-25 01:36:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// const category = 'toys';
+
+// console.log(`http://someurl.com/${category}/5`);
+
+// const surname = "Balashov";
+// alert(`Привет, ${surname}`);
+
+console.log(5 % 2);
+
+console.log(3 * 15 == 45);
+
+const isChecekd = true,
+    isClose = false;
+
+console.log(isChecekd || isCLose);
